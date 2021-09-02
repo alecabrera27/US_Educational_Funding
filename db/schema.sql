f2b3fb5329be993d92b6cc5ee3435d7c277f1045
Kenneth Priester <kenny@tradeblock.us>
2021-09-01 19:51:56 -0500

    created a folder for database related files and started an initial schema file

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "BaseRecord" (
+    "ID" int   NOT NULL,
+    "Year" int   NOT NULL,
+    "State" string   NOT NULL,
+    CONSTRAINT "pk_BaseRecord" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE INDEX "idx_BaseRecord_Year"
+ON "BaseRecord" ("Year");
+
+CREATE INDEX "idx_BaseRecord_State"
+ON "BaseRecord" ("State");
+
